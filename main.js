6723d4d02c1592c50ebcea3e0269e5714702b1e9
SovietOnion1945 <94302456+SovietOnion1945@users.noreply.github.com>
2022-05-24 13:19:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var a = 30
+var b = 5
+
+
+
+
+var add = a+b;
+console.log(add);
+
+
+
+
+var minus = a-b;
+console.log(minus);
+
+
+
+var multiply = a*b;
+console.log(multiply);
+
+
+var divide = a/b;
+console.log(divide);
+
+
+
+
+var first_name = Joseph;
+var second_name = Stalin;
+
+var full_name= first_name.concat(second_name);
+
+console.log(full_name);
+print.console;
+
